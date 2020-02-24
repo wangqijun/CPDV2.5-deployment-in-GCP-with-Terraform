381b9e8a0ede56cd727eb5d9a27e7c8aa25ebc5c
wangqijun <61218608+wangqijun@users.noreply.github.com>
2020-02-24 09:54:10 -0500

    Delete user-setup.sh

--- terraform-tf/helper_scripts/user-setup.sh
+++
@@ -1,6 +0,0 @@
-sudo useradd ocp
-echo "password" |sudo  passwd --stdin ocp
-sudo sed -i "\$aocp ALL=(ALL) NOPASSWD: ALL" /etc/sudoers
-sudo sed -i "s/#PasswordAuthentication yes/PasswordAuthentication yes/g" /etc/ssh/sshd_config
-sudo service sshd restart
- 
