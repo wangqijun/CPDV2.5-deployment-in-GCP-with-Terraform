9e2ad59df7f02b2b9a70fbfc69581b97b73ea6fb
wangqijun <61218608+wangqijun@users.noreply.github.com>
2020-02-25 09:39:04 -0500

    Update register-repo.sh

--- scripts/register-repo.sh
+++ scripts/register-repo.sh
@@ -3,5 +3,8 @@ sudo subscription-manager register --username <redhat register user name> --pass
 sudo subscription-manager refresh
 sudo subscription-manager attach --pool=<pool id>
 sudo subscription-manager repos --disable="*"
+sudo sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/google-cloud.repo
+sudo sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/rh-cloud.repo
+sudo sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo 
 sudo subscription-manager repos     --enable="rhel-7-server-rpms"     --enable="rhel-7-server-extras-rpms"     --enable="rhel-7-server-ose-3.11-rpms"     --enable="rhel-7-server-ansible-2.6-rpms"
 
