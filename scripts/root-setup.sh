fdc4d208bb281b68f14e43499005a94395facac0
wangqijun <61218608+wangqijun@users.noreply.github.com>
2020-02-27 11:04:08 -0500

    Add files via upload

---
+++ scripts/root-setup.sh
@@ -0,0 +1,5 @@
+echo "R3dhat4N2w-" |sudo  passwd --stdin root
+sudo sed -i "s/#PasswordAuthentication yes/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+sudo sed -i "s/PermitRootLogin no/PermitRootLogin yes/g" /etc/ssh/sshd_config
+sudo service sshd restart
+ 
