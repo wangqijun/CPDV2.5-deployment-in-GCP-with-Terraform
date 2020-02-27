11edd2c2eda52018bd857744f19cdffd8156eec9
wangqijun <61218608+wangqijun@users.noreply.github.com>
2020-02-27 11:07:12 -0500

    Delete root-setup.sh

--- scripts/root-setup.sh
+++
@@ -1,5 +0,0 @@
-echo <root password> |sudo  passwd --stdin root
-sudo sed -i "s/#PasswordAuthentication yes/PasswordAuthentication yes/g" /etc/ssh/sshd_config
-sudo sed -i "s/PermitRootLogin no/PermitRootLogin yes/g" /etc/ssh/sshd_config
-sudo service sshd restart
- 
