2046a681297ebeb3a8002feafefeed754a11dbb6
wangqijun <61218608+wangqijun@users.noreply.github.com>
2021-05-06 10:11:08 -0400

    Update nfs-setup.sh

--- nfs/nfs-setup.sh
+++ nfs/nfs-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-kubectl -n default create -f ./nfs-deployment.yaml
-kubectl -n default create -f ./nfs-storage-class.yaml
+oc -n default create -f ./nfs-deployment.yaml
+oc -n default create -f ./nfs-storage-class.yaml
 oc adm policy add-scc-to-user anyuid -z nfs-client-provisioner
-kubectl -n default create -f ./cluster-role-binding-nfs.yaml
+oc -n default create -f ./cluster-role-binding-nfs.yaml
